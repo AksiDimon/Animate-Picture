29950f51dc9ae601493328d44ab56ccd36d081f6
Dmitry Aksenov <aksbum@gmail.com>
2024-11-21 19:11:00 +0200

    added perfict func

--- components/component1.jsx
+++ components/component1.jsx
@@ -1,5 +1,13 @@
 function Component1 () {
+
     const style = { background: "yellow" };
+
+    function perfict (arg) {
+        if(arg % 2 === 0) {
+            return true
+        }
+        return false
+    }
     return (
         <div style={style}>
         Hello component
