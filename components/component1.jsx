2859c582c96a880910f281a6c91787f63c22f196
Синяков Максим <m.sinyakov@cian.ru>
2024-11-21 20:10:24 +0300

    [max] component1 background

--- components/component1.jsx
+++ components/component1.jsx
@@ -1,7 +1,8 @@
 function Component1 () {
+    const style = { background: "yellow" };
     return (
-        <>
+        <div style={style}>
         Hello component
-        </>
+        </div>
     )
 }
