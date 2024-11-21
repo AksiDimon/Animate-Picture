01aff8a6c5b72731b1ff015427ebd95e0e4206a3
Dmitry Aksenov <aksbum@gmail.com>
2024-11-21 19:16:25 +0200

    changed component1, componentMaster, animate.js

--- components/componentMaster.jsx
+++ components/componentMaster.jsx
@@ -1,5 +1,13 @@
-function Master () {
+function Master() {
     return 'origin'
 }
 
 console.log("hello")
+
+function race(arr) {
+    return new Promise((res, rej) => {
+        for (const p of arr) {
+            return p.then((val) => res(val), (err) => rej(err))
+        }
+    })
+}
